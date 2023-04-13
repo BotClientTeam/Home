39f8eb7139847b71c84a4ab9b9c1ae7411c84122
RenaOfficial <y.games@outlook.jp>
2023-04-14 07:34:30 +0900

    ヘッダー作成途中

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head lang="ja">
+    <title>Discord Bot Client for WEB</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="{{ asset("/css/index.css") }}">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="{{ asset("/js/jquery.js") }}"></script>
+</head>
+<body>
+<header class="header">
+    <ul>
+        <li>Bot招待</li>
+        <li>Webアプリ</li>
+        <li>利用規約</li>
+        <li>入力した情報の取り扱いについて</li>
+    </ul>
+</header>
+</body>
+</html>
