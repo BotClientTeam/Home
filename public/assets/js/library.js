87610c8623dd7cacf37e1baa6ab1e5a234274587
RenaOfficial <y.games@outlook.jp>
2023-04-20 19:15:35 +0900

    ヘッダー作成完了

---
+++ public/assets/js/library.js
@@ -0,0 +1,9 @@
+// $(window).on('load', function (){
+//     $(".header").slideToggle()
+//                .toggleClass("active");
+// });
+$(function () {
+    $("header").slideToggle()
+        .toggleClass("active");
+    $("header").css("display", "flex")
+});
--- public/index.php
+++ public/index.php
@@ -1,19 +1,45 @@
-<?php
-require '../lib/definition.php';
-?>
 <!DOCTYPE html>
 <html lang="ja">
-<head lang="ja">
-    <title>Discord Bot Client for WEB</title>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="css/index.css">
+<head prefix="og: https://ogp.me/ns# fb: https://ogp.me/ns/ fb# prefix属性: https://ogp.me/ns/ prefix属性#">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+
+    <noscript>
+        <meta http-equiv="refresh" content="0;URL=error/noscript.html">
+    </noscript>
+
+    <title>Discord BOT Client</title>
+
+    <link rel="apple-touch-icon" sizes="180x180" href="./assets/img/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="./assets/img/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="./assets/img/favicon-16x16.png">
+    <link rel="manifest" href="./assets/img/site.webmanifest">
+    <link rel="mask-icon" href="./assets/img/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="./assets/img/favicon.ico">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-config" content="./assets/img/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+    <meta property="og:url" content="https://app.gakerbot.net/" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="アプリ" />
+    <meta property="og:description" content="Made By Discord BOT Client Team" />
+    <meta property="og:site_name" content="Discord BOT Client" />
+    <meta property="og:image" content="https://app.gakerbot.net/assets/img/favicon.ico" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Discord BOT Client Team">
+    <meta name="keywords" content="Discord,BOT">
+    <link rel="stylesheet" href="assets/css/index.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="js/jquery.js"></script>
+    <script src="assets/js/library.js"></script>
 </head>
 <body>
-<header class="header">
-    <ul>
-        <a href=""><li>Webアプリを開く</li></a>
+<header>
+    <ul class="header_menu">
+        <a href="#"><img src="./assets/img/favicon-32x32.png"></a>
+        <a href="https://app.gakerbot.net/"><li>Webアプリを開く</li></a>
         <a href="terms-of-service.php"><li>利用規約</li></a>
         <a href="about_input_data.php"><li>入力した情報の取り扱いについて</li></a>
     </ul>
